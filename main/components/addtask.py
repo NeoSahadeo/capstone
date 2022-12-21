775a72c68fd51ee4b2adf30d73f13f3a530f6eaa
NeoSahadeo <neosahadeo@protonmail.com>
2022-12-21 14:08:25 +0200

    got the form to communicate with the model

--- main/components/addtask.py
+++ main/components/addtask.py
@@ -1,16 +1,13 @@
 from django import forms
-from django.core import serializers
 from django_unicorn.components import UnicornView
-from .showtasksweekly import referredValue
 
-class GenerateForm(forms.Form):
+class AddTaskForm(forms.Form):
     title = forms.CharField(required=True)
-    
-    def serialize(self):
-        return {
-            'title': self.title
-        }  
 
 class AddtaskView(UnicornView):
-    referredValue = referredValue
-    
+    form_class = AddTaskForm
+    title = ''
+
+    def submit(self):
+        print(self.title)
+        self.validate()
